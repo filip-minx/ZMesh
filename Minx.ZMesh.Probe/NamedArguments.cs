42f46026d2317af38be1cefad34b63a10f0cbb2e
Filip Minx <filip.minx2@gmail.com>
2024-08-06 23:22:18 +0200

    Add probe project

---
+++ Minx.ZMesh.Probe/NamedArguments.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+
+public static class NamedArguments
+{
+    private static readonly Dictionary<string, string> arguments = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+    static NamedArguments()
+    {
+        ParseArguments();
+    }
+
+    private static void ParseArguments()
+    {
+        var namedArgumentRegex = new Regex(@"^\s*--\s*.+");
+
+        var args = Environment.GetCommandLineArgs();
+
+        string name, value;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            name = args[i];
+
+            if (namedArgumentRegex.IsMatch(name))
+            {
+                name = name.Substring(name.IndexOf("--") + 2);
+
+                if ((i < args.Length - 1) && (!namedArgumentRegex.IsMatch(args[i + 1])))
+                {
+                    value = args[i + 1];
+                    i++;
+                }
+                else
+                {
+                    value = "true";
+                }
+
+                arguments[name] = value;
+            }
+        }
+    }
+
+    public static bool TryGetAs<T>(string argName, out T value)
+    {
+        if (!arguments.TryGetValue(argName, out var strValue))
+        {
+            value = default;
+
+            return false;
+        }
+
+        value = (T)Convert.ChangeType(strValue, typeof(T));
+
+        return true;
+    }
+
+    public static T GetAs<T>(string argName, T defaultValue)
+    {
+        if (!TryGetAs(argName, out T value))
+        {
+            return defaultValue;
+        }
+
+        return value;
+    }
+}
---
+++ Minx.ZMesh.Probe/Program.cs
@@ -0,0 +1,60 @@
+﻿using Minx.ZMesh;
+using static NamedArguments;
+
+var map = GetAs("map", "sysmap.yaml");
+
+if (!TryGetAs("messageBox", out string mbName))
+{
+    Console.WriteLine("No message box name provided. Exiting.");
+    return;
+}
+
+using var zmesh = new ZMesh(null, SystemMap.LoadFile(map));
+
+var box = zmesh.At(mbName);
+
+if (GetAs("tell", false))
+{
+    if (TryGetAs("contentType", out string contentType))
+    {
+        string content = "";
+
+        TryGetAs("content", out content);
+
+        if (TryGetAs("contentFile", out string contentFile))
+        {
+            content = File.ReadAllText(contentFile);
+        }
+
+        box.Tell(contentType, content);
+    }
+    else
+    {
+        Console.WriteLine("No content type or content provided. Exiting.");
+    }
+}
+
+if (GetAs("ask", false))
+{
+    if (TryGetAs("contentType", out string contentType))
+    {
+        string content = "";
+
+        TryGetAs("content", out content);
+
+        if (TryGetAs("contentFile", out string contentFile))
+        {
+            content = File.ReadAllText(contentFile);
+        }
+
+        content = content ?? "{}";
+
+        var result = await box.Ask(contentType, content);
+
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("No content type or content provided. Exiting.");
+    }
+}

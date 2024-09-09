3b5a6abb579d3cb20833b6f6ec6832b21c25365f
Filip Minx <filip.minx2@gmail.com>
2024-09-09 22:32:02 +0200

    Add support for custom message handling

--- Minx.ZMesh/MessageBoxProcessor.cs
+++ Minx.ZMesh/MessageBoxProcessor.cs
@@ -9,16 +9,17 @@ namespace Minx.ZMesh
     public class MessageBoxProcessor : IMessageBoxProcessor, IDisposable
     {
         private readonly IMessageBox messageBox;
-
+        private readonly MessageProcessingOptions options;
         private readonly Dictionary<string, Action<object>> tellHandlers = new Dictionary<string, Action<object>>();
         private readonly Dictionary<string, Func<object, object>> questionHandlers = new Dictionary<string, Func<object, object>>();
         private readonly Channel<(MessageType messageType, string contentType)> messageChannel;
 
         private bool isDisposed;
 
-        public MessageBoxProcessor(IMessageBox messageBox)
+        public MessageBoxProcessor(IMessageBox messageBox, MessageProcessingOptions options = null)
         {
             this.messageBox = messageBox;
+            this.options = options ?? new MessageProcessingOptions();
 
             messageChannel = Channel.CreateUnbounded<(MessageType, string)>();
 
@@ -68,11 +69,11 @@ namespace Minx.ZMesh
             switch (messageType)
             {
                 case MessageType.Tell:
-                    messageBox.TryListen(contentType, tellHandlers[contentType]);
+                    HandleTellMessage(contentType);
                     break;
 
                 case MessageType.Question:
-                    messageBox.TryAnswer(contentType, questionHandlers[contentType]);
+                    HandleQuestionMessage(contentType);
                     break;
 
                 default:
@@ -80,14 +81,60 @@ namespace Minx.ZMesh
             }
         }
 
+        private void HandleQuestionMessage(string contentType)
+        {
+            if (questionHandlers.TryGetValue(contentType, out var handler))
+            {
+                messageBox.TryAnswer(contentType, handler);
+            }
+            else
+            {
+                if (options.OnMissingHandler == null)
+                {
+                    throw new InvalidOperationException($"No handler for message type: {contentType}");
+                }
+
+                options.OnMissingHandler?.Invoke((MessageType.Question, contentType));
+            }
+        }
+
+        private void HandleTellMessage(string contentType)
+        {
+            if (tellHandlers.TryGetValue(contentType, out var handler))
+            {
+                messageBox.TryListen(contentType, handler);
+            }
+            else
+            {
+                if (options.OnMissingHandler == null)
+                {
+                    throw new InvalidOperationException($"No handler for message type: {contentType}");
+                }
+
+                options.OnMissingHandler?.Invoke((MessageType.Tell, contentType));
+            }
+        }
+
         public void Listen<TMessage>(Action<TMessage> handler)
         {
+            ThrowIfArgumentIsNull(handler, nameof(handler));
+
             tellHandlers[typeof(TMessage).Name] = message => handler((TMessage)message);
         }
 
         public void Answer<TQuestion, TAnswer>(Func<TQuestion, TAnswer> handler)
         {
+            ThrowIfArgumentIsNull(handler, nameof(handler));
+
             questionHandlers[typeof(TQuestion).Name] = message => handler((TQuestion)message);
+        }
+
+        private static void ThrowIfArgumentIsNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
         }
 
         public void Dispose()
---
+++ Minx.ZMesh/MessageProcessingOptions.cs
@@ -0,0 +1,10 @@
+﻿using Minx.ZMesh.Models;
+using System;
+
+namespace Minx.ZMesh
+{
+    public class MessageProcessingOptions
+    {
+        public Action<(MessageType messageType, string contentType)> OnMissingHandler { get; set; } = null;
+    }
+}
